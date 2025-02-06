2b536bc127dbab2805a8ed71f1172ee7362663b9
Pankaj Kumar <dev.pankajkr7@gmail.com>
2025-02-06 20:41:05 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Wheat from './Components/Wheat'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Wheat></Wheat>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Wheat.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Wheat = () => {
+  return (
+    <div className='weather'>
+        <div className="search-bar">
+            <input type="text" placeholder='Search' />
+            <img src="" alt="" />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Wheat
